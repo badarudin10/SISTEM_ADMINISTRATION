3398f74435e8e38167d9a339365e4abecdccb619
badarudin10 <badaraddin249@gmail.com>
2026-08-30 20:26:52 +0700

    Delete vitest.ts

--- vitest.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// Pastikan TIDAK ADA opsi 'base' di sini
-export default defineConfig({
-  plugins: [react()],
-});
