8e6baa529efd04c27f4bb99572537472779dc721
badarudin10 <badaraddin249@gmail.com>
2026-08-30 14:34:37 +0700

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/SISTEM_ADMINISTRATION/', // Wajib sama dengan nama repository GitHub Anda
-});
-
